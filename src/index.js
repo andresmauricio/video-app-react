d0345845f961f1dec8741dce05a0a8d4dcf85151
andresmauricio <andresacelas@outlook.com>
2020-08-06 22:45:42 -0500

    Configuracion Redux React.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './routes/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 ReactDOM.render(
-  <App />,
+  <Provider>
+    <App />
+  </Provider>,
   document.getElementById('app')
 );
